19f2ed61e30f0f6196c8b02bbcbc14a06a22e850
kiransinghsingh9336 <kiransinghsingh9336@gmail.com>
2026-06-29 10:25:51 +0530

    CLASS WORK

---
+++ CLASS WORK/CW1.js
@@ -0,0 +1,22 @@
+// create function to print 5 to 75 numbers.
+function numbers(){
+    for(let i=5;i<=75;i++){
+        console.log(i);
+    }
+}
+numbers()
+
+
+// create a function to find out Remainder
+function rem(){
+    let num1 = 15;
+    let num2 = 4;
+    num = num1%num2;
+    console.log("remainder=",num);
+}
+
+rem();
+
+
+
+
