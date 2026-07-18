4573a41014d3a062ae5acf6651e895f1eda36112
kiransinghsingh9336 <kiransinghsingh9336@gmail.com>
2026-07-18 15:53:16 +0530

    Update project

--- express.js
+++ express.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import { log } from 'node:console';
 const app=express()
 
-app.listen(3000,()=>{
+app.listen(5000,()=>{
     console.log("server is running at port no:5000"); 
 })
 app.get('/',(req,res)=>{
@@ -20,5 +21,20 @@ app.post('/Recieving',(req,res)=>{
     res.send("This is my recieving view page here");
 })
 
+// app.use(express.json())
+// app.post('/user',(req,res)=>{
+//     console.log(req.body);
+//     res.send(req.body)
+// })
 
+app.use(express.urlencoded({ extended: true })); //http://localhost:5000/signin
+app.use(express.json());
+
+app.post('/signin', (req, res) => {
+    console.log(req.body);
+    res.send({
+        message: "Data is send",
+        Data: req.body
+    });
+});
 
